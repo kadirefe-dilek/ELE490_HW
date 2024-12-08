5e487a62835f94c534b21fbd90b0309675897dae
Kadir Efe Dilek <kadirefe06682@gmail.com>
2024-12-08 15:21:58 +0300

    Solved for Q2

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -11,6 +11,20 @@ from scipy import signal
 
 from os import getcwd as gcd
 
+import numpy as np
+
+"""
+Generates zero-mean Gaussian random noise for a given image dimension.
+    in:
+        dimensions: Tuple indicating the dimensions of the image (N, M).
+        sigma: Standard deviation of the Gaussian noise.
+    out:
+        noise: A 2D numpy array of Gaussian noise.
+"""
+def generateGaussianNoise(dimensions, sigma):
+    N, M = dimensions
+    noise = np.random.normal(loc=0, scale=sigma, size=(N, M))  # Zero-mean Gaussian noise
+    return noise
 
 ## Definitions and reading the image
 # Define path and name to read the image
@@ -37,3 +51,36 @@ plt.show()
 
 # Scale the image into the range [0-1] 
 c = (arr_Cameraman - arr_Cameraman.min()) / (arr_Cameraman.max() - arr_Cameraman.min())
+
+## Q2 - Obtain Gaussian-noised images with different sigma values 
+
+# Display the noise for sigma=25 
+N,M = arr_Cameraman.shape[0], arr_Cameraman.shape[1]
+imgDimensions = (N,M)
+sigma_test = 25  # Standard deviation for the noise
+
+arr_gaussianNoise = generateGaussianNoise(imgDimensions, sigma_test)
+
+# Display the generated noise
+plt.figure()
+plt.title(f"Gaussian Random Noise (sigma={sigma_test})")
+plt.imshow(arr_gaussianNoise, cmap='gray')
+plt.colorbar(label="Noise Intensity")
+plt.imsave(str(imageRootPath + fileSep + 'img_GaussianNoise_sigma_' + str(int(sigma_test)) + '.jpg'), arr_gaussianNoise, cmap='gray')
+plt.axis('off')
+plt.show()
+
+sigmaVals = np.array([10,100,500,1000]) / 1000
+Nc = np.zeros((N,M,sigmaVals.shape[0]))
+
+for i,sigma in enumerate(sigmaVals):
+    Nc[:,:,i] = c + generateGaussianNoise(imgDimensions, sigma)
+    img_Nc = 255 * Nc # its inefficient to multiply the whole array each time
+    # Display the noisy image
+    plt.figure()
+    plt.title(f"Image With Gaussian Random Noise (sigma={sigma})")
+    plt.imshow(Nc[:,:,i], cmap='gray')
+    # plt.colorbar(label="Noise Intensity")
+    plt.savefig(str(imageRootPath + fileSep + 'fig_noisyCameraman_sigma_' + str(int(sigma*1000)) + 'xE-3.jpg'))
+    plt.axis('off')
+    plt.show()
